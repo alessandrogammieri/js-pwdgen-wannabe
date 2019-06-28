99723d5f18f0514a2476124fb59615645af73521
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-06-28 19:27:40 +0200

    Update myscript.js
    add variables name + surname + color

--- myscript.js
+++ myscript.js
@@ -1,1 +1,8 @@
-/* Password generator! */
+// Variabili Password generator!
+var name, surname, color;
+
+// Stringhe Nome, Cognome e Colore
+var name = prompt("Inserisci il tuo nome");
+var surname = prompt("Inserisci il tuo cognome");
+var color = prompt("Dimmi il tuo colore preferito");
+alert(name + surname + color + 19);

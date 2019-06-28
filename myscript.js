90139071274724d78336696a6dde0733d51049f2
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-06-28 19:28:11 +0200

    Revert "Update myscript.js"
    This reverts commit 99723d5f18f0514a2476124fb59615645af73521.

--- myscript.js
+++ myscript.js
@@ -1,8 +1,1 @@
-// Variabili Password generator!
-var name, surname, color;
-
-// Stringhe Nome, Cognome e Colore
-var name = prompt("Inserisci il tuo nome");
-var surname = prompt("Inserisci il tuo cognome");
-var color = prompt("Dimmi il tuo colore preferito");
-alert(name + surname + color + 19);
+/* Password generator! */
